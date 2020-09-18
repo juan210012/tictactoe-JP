3021e47a6e2a9a9ac1d8d517071f69283e1694c7
juan210012 <65736594+juan210012@users.noreply.github.com>
2020-09-18 13:01:01 -0400

    Delete connect.php

--- PHP/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connect.php
-try { 
-        $dbConn = new  PDO("mysql:host=localhost:3306;dbname=estupiju_tictactoe", "estupiju_guest", "phpGuest_2020"); 
-    } catch (PDOException $e) { 
-        echo "Connection error: " . $e->getMessage(); 
-    }
-?>
